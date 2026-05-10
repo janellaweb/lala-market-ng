ecc88e314e1d4bd3b871076d8ac094480c97d35d
Janella Web <quizonwebsites@gmail.com>
2026-05-09 22:32:34 -0600

    Added Product Card Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/header/header";
+import { Product } from './market/product';
+import { ProductCard } from './market/product-card/product-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, ProductCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'lala-market-ng';
+  protected readonly title = 'lala-market-ng';
+
+product: Product = {
+
+    id: 1,
+    price: 9.99,
+    name: 'Test Product',
+    description: 'This is a test product.',
+    image: 'sample-product.jpg'
+  };
+
 }
